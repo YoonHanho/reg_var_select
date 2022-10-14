de6ff295752816e9454ea647aba9db7a83608db0
윤한호 <hanhoman@gmail.com>
2022-10-14 19:46:33 +0900

    Add files via upload

---
+++ reg_var_select.py
@@ -0,0 +1,75 @@
+# 회귀분석 변수선택 파이썬 코드 : ADP 파이썬 교재 
+# https://github.com/ADPclass/ADP_book_ver01/ >> 7장 5절 회귀분석
+# https://bit.ly/3CqoRoY
+
+import time
+import itertools
+def processSubset(X,y, feature_set):
+            model = sm.OLS(y,X[list(feature_set)]) # Modeling
+            regr = model.fit() # 모델 학습
+            AIC = regr.aic # 모델의 AIC
+            return {"model":regr, "AIC":AIC}
+# 전진선택법
+def forward(X, y, predictors):
+    # 데이터 변수들이 미리정의된 predictors에 있는지 없는지 확인 및 분류
+    remaining_predictors = [p for p in X.columns.difference(['Intercept']) if p not in predictors]
+    results = []
+    for p in remaining_predictors:
+        results.append(processSubset(X=X, y= y,            feature_set=predictors+[p]+['Intercept']))
+        
+    # 데이터프레임으로 변환
+    models = pd.DataFrame(results)
+    # AIC가 가장 낮은 것을 선택
+    best_model = models.loc[models['AIC'].argmin()] # index
+    print("Processed ", models.shape[0], "models on", len(predictors)+1, "predictors in")
+    print('Selected predictors:',best_model['model'].model.exog_names,' AIC:',best_model[0] )
+    return best_model
+	
+# 후진소거법
+def backward(X,y,predictors):
+    tic = time.time()
+    results = []
+    
+    # 데이터 변수들이 미리정의된 predictors 조합 확인
+    for combo in itertools.combinations(predictors, len(predictors) -1):
+        results.append(processSubset(X=X, y= y,        feature_set = list(combo)+['Intercept']))
+    models = pd.DataFrame(results)
+    
+    # 가장 낮은 AIC를 가진 모델을 선택
+    best_model = models.loc[models['AIC'].argmin()]
+    toc = time.time()
+    print("Processed ", models.shape[0], "models on",          len(predictors) -1, "predictors in", (toc - tic))
+    print('Selected predictors:',best_model['model'].model.exog_names,         'AIC:',best_model[0] )
+
+    return best_model	
+	
+# 단계적 선택법
+def Stepwise_model(X,y):
+    Stepmodels = pd.DataFrame(columns=["AIC", "model"])
+    tic = time.time()
+    predictors = []
+    Smodel_before = processSubset(X,y,predictors+['Intercept'])['AIC']
+
+    for i in range(1, len(X.columns.difference(['Intercept'])) +1):
+        Forward_result = forward(X=X, y=y, predictors=predictors) 
+        print('forward')
+        Stepmodels.loc[i] = Forward_result
+        predictors = Stepmodels.loc[i]["model"].model.exog_names
+        predictors = [ k for k in predictors if k !='Intercept']
+        Backward_result = backward(X=X, y=y, predictors=predictors)
+
+        if Backward_result['AIC']< Forward_result['AIC']:
+            Stepmodels.loc[i] = Backward_result
+            predictors = Stepmodels.loc[i]["model"].model.exog_names
+            Smodel_before = Stepmodels.loc[i]["AIC"]
+            predictors = [ k for k in predictors if k !='Intercept']
+            print('backward')
+
+        if Stepmodels.loc[i]['AIC']> Smodel_before:
+            break
+        else:
+            Smodel_before = Stepmodels.loc[i]["AIC"]
+    toc = time.time()
+    print("Total elapsed time:", (toc - tic), "seconds.")
+
+    return (Stepmodels['model'][len(Stepmodels['model'])])	
